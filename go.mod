308d71720da31e2e6fb7f31c98aa087bdd61ed54
s2422114 <s2422114@stu.musashino-u.ac.jp>
2026-08-02 18:32:30 +0900

    Add moretypes/17_exercise_slices (Pic function)

--- go.mod
+++ go.mod
@@ -1,3 +1,5 @@
 module learn_go
 
 go 1.26.5
+
+require golang.org/x/tour v0.1.0 // indirect
---
+++ moretypes/17_exercise_slices/main.go
@@ -0,0 +1,19 @@
+package main
+
+import "golang.org/x/tour/pic"
+
+func Pic(dx, dy int) [][]uint8 {
+	result := make([][]uint8, dy)
+
+	for y := 0; y < dy; y++ {
+		result[y] = make([]uint8, dx)
+		for x := 0; x < dx; x++ {
+			result[y][x] = uint8((x + y) / 2)
+		}
+	}
+	return result
+}
+
+func main() {
+	pic.Show(Pic)
+}
